57a9a1817f9fdf8e7d3996ab633a069cf844f62a
Fedi <fedi.benromdhane@esprit.tn>
2021-10-24 12:02:27 +0100

    new

--- jeux.cpp
+++ jeux.cpp
@@ -1,6 +1,24 @@
 #include "jeux.h"
-
-Jeux::Jeux()
+#include<iostream>
+Jeux::Jeux(int ID,QString nation,QString nom_jeux,int nb_jeux)
 {
-
+    this->nation=nation;
+    this->nom_jeux=nom_jeux;
+    this->nb_jeux=nb_jeux;
+    this->ID=ID;
 }
+
+/*bool Jeux::ajouter()
+{
+  //to do next
+}
+
+QSqlQueryModel * Jeux::afficher()
+{
+   //to do next
+}
+bool Jeux::supprimer(int)
+{
+   //to do next
+}
+*/
